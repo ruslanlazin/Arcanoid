0388f69d50a4e7e32edc00dfb6bb376562292f4e
ruslanlazin <ruslanlazin@gmail.com>
2016-10-03 12:43:36 +0300

    Reliase 1.0

--- src/Game.js
+++ src/Game.js
@@ -18,18 +18,28 @@ var lives = 10;
 var isRightPressed = false;
 var isLeftPressed = false;
 var racket = {width: 150, height: 20, x: canvas.width / 2 - 75, y: canvas.height - 20};
+var gameIsActive = true;
+
+buildWall();
+drawScores();
+
+var intervalID = setInterval(function () {
+    makeFrame();
+}, 1);
 
 
-for (x = offSet; x < canvas.width - brickWidth; x += offSet + brickWidth) {
-    for (y = offSet + scoreTableHeight; y < wallHeight; y += offSet + brickHeight) {
-        bricks.push({x: x, y: y});
+//functions
+function buildWall() {
+    for (x = offSet; x < canvas.width - brickWidth; x += offSet + brickWidth) {
+        for (y = offSet + scoreTableHeight; y < wallHeight; y += offSet + brickHeight) {
+            bricks.push({x: x, y: y});
+        }
     }
+    bricks.forEach(function (brick) {
+        ctx.fillStyle = "#" + Math.floor((1 << 24) * Math.random()).toString(16);
+        ctx.fillRect(brick.x, brick.y, brickWidth, brickHeight);
+    });
 }
-
-bricks.forEach(function (brick) {
-    ctx.fillStyle = "#" + Math.floor((1 << 24) * Math.random()).toString(16);
-    ctx.fillRect(brick.x, brick.y, brickWidth, brickHeight);
-});
 
 function drawScores() {
     ctx.fillStyle = '#0bb';
@@ -38,29 +48,21 @@ function drawScores() {
     ctx.fillStyle = "#000";
     ctx.fillText("Lives: " + lives, canvas.width - 100, 20);
     ctx.fillText("Scores: " + score, canvas.width - 250, 20);
-    ctx.fillText("press space to restart the game ", 50, 20);
+    ctx.fillText("Press space to restart the game ", 50, 20);
 }
 
 
-function drawRacket(racket) {
+function drawRacket() {
+
     if (isRightPressed === true && racket.x < canvas.width - racket.width) {
-        ctx.clearRect(racket.x, racket.y, racket.width, racket.height);
         racket.x += 5;
-        ctx.fillStyle = '#0bb';
-        ctx.fillRect(racket.x, racket.y, racket.width, racket.height);
     } else if (isLeftPressed === true && racket.x > 0) {
-        ctx.clearRect(racket.x, racket.y, racket.width, racket.height);
         racket.x -= 5;
-        ctx.fillStyle = '#0bb';
-        ctx.fillRect(racket.x, racket.y, racket.width, racket.height);
     }
+    ctx.clearRect(0, racket.y, canvas.width, racket.height);
+    ctx.fillStyle = '#0bb';
+    ctx.fillRect(racket.x, racket.y, racket.width, racket.height);
 }
-
-
-var intervalID = setInterval(function () {
-    makeFrame();
-}, 1);
-
 
 function makeFrame() {
     eraseBall(ball.x, ball.y);
@@ -68,7 +70,7 @@ function makeFrame() {
     ball.y += ball.yVelocity;
     detectCollision(ball);
     drawBall(ball.x, ball.y);
-    drawRacket(racket);
+    drawRacket();
 }
 
 function remove(bricknumber) {
@@ -93,21 +95,13 @@ function eraseBall(x, y) {
     ctx.closePath();
 }
 
-function detectCollision(ball) {
+function detectCollision() {
     if (ball.x > canvas.width - ballRadius || ball.x < ballRadius) {
         ball.xVelocity = -ball.xVelocity;
     }
-    if (ball.y > canvas.height - ballRadius || ball.y < ballRadius + scoreTableHeight) {
+    if (ball.y < ballRadius + scoreTableHeight) {
         ball.yVelocity = -ball.yVelocity;
-        // soundBoom();
-        soundPing();
-        lives--;
-        if (lives === 0) {
-            lose();
-        }
-        drawScores();
     }
-
     for (i = 0; i < bricks.length; i++) {
         brick = bricks[i];
         if ((ball.x > brick.x) && (ball.x < brick.x + brickWidth) && (ball.y < brick.y + brickHeight + ballRadius)) {
@@ -122,17 +116,52 @@ function detectCollision(ball) {
             break;
         }
     }
+    if (ball.y > canvas.height - ballRadius) {
+        ball.yVelocity = -ball.yVelocity;
+        if (gameIsActive) {
+            soundBoom();
+            lives--;
+            drawScores();
+            if (lives === 0) {
+                lose();
+            }
+        }
+    }
+    if (ball.y > racket.y - ballRadius && ball.x - ballRadius > racket.x && ball.x < racket.x + racket.width + ballRadius) {
+        ball.yVelocity = -Math.abs(ball.yVelocity);
+        if (gameIsActive) {
+            soundPing();
+        }
+    }
 }
+
+function win() {
+    gameIsActive = false;
+    soundWin();
+    var i = 1;
+    setInterval(function () {
+        ctx.fillStyle = '#0bb';
+        ctx.fillRect(0, 0, canvas.width, 20 + i++);
+        ctx.fillStyle = "#a00";
+        ctx.font = "24px Times";
+        ctx.fillText("Press space to restart the game ", 50, 20);
+        ctx.font = '' + i + 'px Times';
+        ctx.fillText("YOU WIN", 30, i);
+    }, 150);
+}
+function lose() {
+    clearInterval(intervalID);
+    alert('Sorry, but you lose. Try again!');
+    document.location.reload();
+}
+
 function randomDirection() {
     return Math.random() * 0.5 - 0.5;  //return (-0.25..0.25)
 }
-
 function mouseMoveHandler(event) {
     var relativeX = event.clientX - canvas.offsetLeft;
     if (relativeX > 0 && relativeX < canvas.width) {
         racket.x = relativeX - racket.width / 2;
-        drawRacket(racket);
-
     }
 }
 function keyDownHandler(event) {
@@ -143,7 +172,6 @@ function keyDownHandler(event) {
         isLeftPressed = true;
     }
 }
-
 function keyUpHandler(event) {
     if (event.keyCode == 39) {
         isRightPressed = false;
@@ -155,30 +183,21 @@ function keyUpHandler(event) {
         document.location.reload();
     }
 }
-
-function win() {
-    //  clearInterval(intervalID);
-    var audio = new Audio();
-    audio.src = 'Queen – We Are the Champions.mp3';
-    audio.autoplay = true;
-
-    var i = 1;
-    setInterval(function () {
-        ctx.fillStyle = '#0bb';
-        ctx.fillRect(0, scoreTableHeight, canvas.width, 20 + i++);
-        ctx.fillStyle = "#a00";
-        ctx.font = '' + i + 'px Times';
-        ctx.fillText("YOU WIN", 30, i + 23);
-    }, 150);
-}
-
 function soundBoom() {
     var audio = new Audio();
     audio.src = 'steelsword.mp3';
     audio.autoplay = true;
+    audio.volume = 0.2;
 }
 function soundPing() {
     var audio = new Audio();
     audio.src = 'woodbat.mp3';
     audio.autoplay = true;
+    audio.volume = 0.05;
+}
+function soundWin() {
+    var audio = new Audio();
+    audio.src = 'Queen – We Are the Champions.mp3';
+    audio.autoplay = true;
+    audio.volume = 1;
 }
